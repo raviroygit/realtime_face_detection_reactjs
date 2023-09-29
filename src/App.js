a41d810e6e298b78246b9f8d68e8971fe0f78b45
roydipoletechi <112629869+roydipoletechi@users.noreply.github.com>
2023-09-29 12:19:02 +0530

    face detection in reactjs

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FaceDetection from './faceDetection';
+
+function App() {
+  return (
+    <div className="App">
+      <FaceDetection />
+    </div>
+  );
+}
+
+export default App;
